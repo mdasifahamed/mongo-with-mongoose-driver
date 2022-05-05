6772c01b51649123fb52e11d73d739001061d64c
asif <mdasifahamed89@yahoo.com>
2022-05-05 20:07:25 +0600

    Thsi is occuerd becasuse i have terminated the server before executing second table  MongoExpiredSessionError: Cannot use a session that has ended     at applySession (E:\mongo with native driver\node_modules\mongodb\lib\sessions.js:655:16)     at Connection.command (E:\mongo with native driver\node_modules\mongodb\lib\cmap\connection.js:271:53)     at E:\mongo with native driver\node_modules\mongodb\lib\sdam\server.js:198:18     at Object.callback (E:\mongo with native driver\node_modules\mongodb\lib\cmap\connection_pool.js:267:13)     at E:\mongo with native driver\node_modules\mongodb\lib\cmap\connection_pool.js:475:29     at E:\mongo with native driver\node_modules\mongodb\lib\cmap\connection_pool.js:403:13     at callback (E:\mongo with native driver\node_modules\mongodb\lib\cmap\connect.js:67:9)     at E:\mongo with native driver\node_modules\mongodb\lib\cmap\connect.js:135:13     at Connection.onMessage (E:\mongo with native driver\node_modules\mongodb\lib\cmap\connection.js:215:9)     at MessageStream.<anonymous> (E:\mongo with native driver\node_modules\mongodb\lib\cmap\connection.js:63:60) {   [Symbol(errorLabels)]: Set(0) {} }

--- app.js
+++ app.js
@@ -1,49 +1,49 @@
 const mongoose = require('mongoose');
 mongoose.connect("mongodb://localhost:27017/fruitsDB");
 const fruitsSchema = new mongoose.Schema({
-  name : String,
-  rating : Number,
-  review : String
+  name: String,
+  rating: Number,
+  review: String
 })
 const Fruit = mongoose.model("Fruit", fruitsSchema);
 const fruit = new Fruit({
-  name : "Apple",
-  rating : 7,
-  review : "Better"
+  name: "Apple",
+  rating: 7,
+  review: "Better"
 });
 
 
 // fruit.save();
 
 const PersonsSchema = new mongoose.Schema({
-  name : String,
-  age : Number
+  name: String,
+  age: Number
 });
 
 const Person = mongoose.model("Person", PersonsSchema);
 const person = new Person({
   name: "John Doe",
-  age : 37
+  age: 37
 })
 // person.save();
 
 // Bulk insert
 
 const banana = new Fruit({
-  name : "Banana",
-  rating : 7,
-  review : "Weired"
+  name: "Banana",
+  rating: 7,
+  review: "Weired"
 })
 
 const orange = new Fruit({
-  name : "Orange",
-  rating : 8,
-  review : "Tangy And Tasty"
+  name: "Orange",
+  rating: 8,
+  review: "Tangy And Tasty"
 })
 const kiwi = new Fruit({
-  name : "Kiwi",
-  rating : 10,
-  review : "Best"
+  name: "Kiwi",
+  rating: 10,
+  review: "Best"
 })
 
 // Fruit.insertMany([banana, orange, kiwi], function(error){
@@ -54,39 +54,46 @@ const kiwi = new Fruit({
 //   }
 // })
 const asif = new Person({
-  name : "Asif",
-  age : 24
+  name: "Asif",
+  age: 24
 })
 
 const rahul = new Person({
-  name : "Rahul",
-  age : 27
+  name: "Rahul",
+  age: 27
 })
 
 const rahim = new Person({
-  name : "Rahim",
-  age : 22
+  name: "Rahim",
+  age: 22
 })
- // Person.insertMany([asif, rahul, rahim], function(error){
- //   if(error){
- //     console.log(error);
- //   }else{
- //     console.log("Successfully Updated The Person Database");
- //   }
- // })
+// Person.insertMany([asif, rahul, rahim], function(error){
+//   if(error){
+//     console.log(error);
+//   }else{
+//     console.log("Successfully Updated The Person Database");
+//   }
+// })
 
- Fruit.find(function(err, fruits){
-   if(err){
-     console.log(err);
-   }else{
-     console.log(fruits);
-   }
- });
+Fruit.find(function(err, fruits) {
+  if (err) {
+    console.log(err);
+  } else {
+    mongoose.connection.close() // to Shut down the server no error and ehat we to get after getting this close method should be called thats it this is good practise 
+    fruits.forEach(function(fruit){
+    console.log(fruit.name);
+    });
+  }
+});
 
- Person.find(function(err, people){
-   if(err){
-     console.log(err);
-   }else{
-     console.log(people);
-   }
- });
+Person.find(function(err, people) {
+  if (err) {
+    console.log(err);
+  } else {
+    mongoose.connection.close() // to Shut down the server no error and ehat we to get after getting this close method should be called thats it this is good practise  .
+
+    people.forEach(function(person){
+    console.log(person.name);
+    });
+  }
+});
